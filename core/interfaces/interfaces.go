9bf6597e9da0e1ac05f86dc3064f1af7797d362c
rayyh <rayyounghong@gmail.com>
2021-07-14 00:35:00 +0800

    go interface review

--- core/interfaces/interfaces.go
+++ core/interfaces/interfaces.go
@@ -7,18 +7,6 @@ import (
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// 虽然 Go 中没有类和继承的概念，但是 Go 语言里有非常灵活的接口概念，通过它可以实现很多面向对象的特性。
-// 接口定义了一组不包含实现的抽象方法，Go 语言中的接口都很简短，通常它们只会包含 0~3 个方法。
-// 在 Go 的底层，接口可以被认为一个是一个包含了值和类型的元组：(value, type)。
-// 接口值保存了一个具体底层类型的具体值，接口值调用方法时会执行其底层类型的同名方法。
-
-// OO 语言最重要的三个方面分别是：封装，继承和多态
-// 封装 -> 和别的 OO 语言有 4 个或更多的访问层次相比，Go 把它简化为了 2 层：当前包范围内 (首字母小写) 和可导出的 (首字母大写)
-// 继承 -> 用组合实现：内嵌一个或多个包含想要的行为 (字段和方法) 的类型，多重继承可以通过内嵌多个类型实现
-// 多态 -> 用接口实现：某个类型的实例可以赋给它所实现的任意接口类型的变量。类型和接口是松耦合的，并且多重继承可以通过实现多个接口实现
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 type Abser interface{ Abs() float64 }
 
 type Vertex struct{ X, Y float64 }
@@ -37,16 +25,10 @@ func (mf MyFloat64) Abs() float64 {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// 接口
-
 type Shaper interface{ Area() float64 }
-
-/// 结构体
 
 type Square struct{ side float64 }
 type Rectangle struct{ length, width float64 }
-
-/// 实现接口
 
 func (sq *Square) Area() float64 {
 	return sq.side * sq.side
@@ -56,10 +38,7 @@ func (rec *Rectangle) Area() float64 {
 	return rec.width * rec.length
 }
 
-/// 接口作为参数类型
-
 func GetType(shaper Shaper) string {
-	// .(type) 关键字可以获取变量的类型
 	switch shaper.(type) {
 	case *Rectangle:
 		return "Rec"
@@ -73,9 +52,6 @@ func GetType(shaper Shaper) string {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// 一个接口可以包含一个或多个其他的接口，这相当于直接将这些内嵌接口的方法列举在外层接口中一样
-// 比如接口 File 包含了 ReadWrite 和 Lock 的所有方法，它还额外有一个 Close() 方法
 
 type ReadWrite interface {
 	Read(b bytes.Buffer) bool
@@ -103,7 +79,6 @@ func (l List) Len() int {
 	return len(l)
 }
 
-// Append 定义在指针类型上
 func (l *List) Append(val int) {
 	*l = append(*l, val)
 }
@@ -112,7 +87,6 @@ type Appender interface {
 	Append(int)
 }
 
-// CountInto 定义在值类型上
 func CountInto(a Appender, start, end int) {
 	for i := start; i <= end; i++ {
 		a.Append(i)
@@ -123,7 +97,6 @@ type Lener interface {
 	Len() int
 }
 
-// LongEnough 定义在值类型上
 func LongEnough(l Lener) bool {
 	return l.Len()*10 > 42
 }
@@ -131,8 +104,6 @@ func LongEnough(l Lener) bool {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Vector 中存储的所有元素都是 Element 类型，要得到它们的原始类型需要用到类型断言
 
 type Element interface{}
 type Vector struct {
--- core/interfaces/interfaces_test.go
+++ core/interfaces/interfaces_test.go
@@ -15,10 +15,7 @@ func TestEmptyInterfaceHoldsValueOfAnyType(t *testing.T) {
 	assert.Equal(t, fmt.Sprintf("(%v, %T)", i, i), "(hello, string)")
 }
 
-// i.(type) 可以检查变量是否为指定类型
 func TestTypeAssertions(t *testing.T) {
-	// 一个接口类型的变量 varI 中可以包含任何类型的值，必须有一种方式来检测它的动态类型，即运行时在变量中存储的值的实际类型
-	// 通常我们可以使用 类型断言 来测试在某个时刻 varI 是否包含类型 T 的值：v := varI.(T)
 	t.Run("type determination", func(t *testing.T) {
 		var i interface{} = "hello"
 		s := i.(string)
@@ -33,7 +30,6 @@ func TestTypeAssertions(t *testing.T) {
 	t.Run("type switch", func(t *testing.T) {
 		classifier := func(items ...interface{}) (res string) {
 			// A type switch is a construct that permits several type assertions in series.
-			// 可以用 type-switch 进行运行时类型分析，但是在 type-switch 不允许有 fallthrough
 			for i, x := range items {
 				switch x.(type) {
 				case bool:
@@ -68,7 +64,6 @@ func TestAbser(t *testing.T) {
 	assert.Equal(t, float64(2), a.Abs())
 	assert.Equal(t, "(-2, interfaces.MyFloat64)", fmt.Sprintf("(%v, %T)", a, a))
 
-	// 智能指针
 	a = &v // a *Vertex (v Vertex) implements Abser
 	assert.Equal(t, float64(5), a.Abs())
 	assert.Equal(t, float64(5), v.Abs())
@@ -81,7 +76,6 @@ func TestShaper(t *testing.T) {
 		s := &Square{5.0}
 		r := &Rectangle{length: 5.0, width: 5.0}
 
-		// Shaper 切片可以容纳 Square 和 Rectangle 类型，因为这两个结构体都实现了 Shaper 接口包含的方法集
 		shapes := []Shaper{s, r}
 
 		for _, shape := range shapes {
@@ -92,7 +86,7 @@ func TestShaper(t *testing.T) {
 	t.Run("check type", func(t *testing.T) {
 		var s Shaper
 		s = new(Rectangle)
-		// 这里必须用 *Square 而不是 Square，因为 Area() 方法的接收者是 *Square 而不是 Square，这与方法调用时的智能指针有所不同
+		// must be *Square, not Square, since Area() receiver is of *Square type
 		_, ok := s.(*Square)
 		assert.False(t, ok)
 		_, ok = s.(*Rectangle)
@@ -107,12 +101,10 @@ func TestShaper(t *testing.T) {
 		assert.Equal(t, "Squ", GetType(s2))
 	})
 
-	// 测试一个值是否实现了某个接口
 	t.Run("check interface", func(t *testing.T) {
 		var s interface{}
 		s = &Rectangle{length: 20, width: 20}
 
-		// 检查 s 是否实现了 Shaper 接口
 		if sv, ok := s.(Shaper); ok {
 			assert.Equal(t, float64(400), sv.Area())
 		}
@@ -120,16 +112,6 @@ func TestShaper(t *testing.T) {
 }
 
 func TestMethodSets(t *testing.T) {
-	// 在接口上调用方法时，必须有和方法定义时相同的接收者类型或者是可以从具体类型 P 直接可以辨识的
-	// -> 指针方法可以通过指针调用
-	// -> 值方法可以通过值调用
-	// -> 接收者是值的方法可以通过指针调用，因为指针会首先被解引用
-	// -> 接收者是指针的方法不可以通过值调用，因为存储在接口中的值没有地址
-
-	// Go 语言规范定义了接口方法集的调用规则
-	// -> 类型 T 的可调用方法集包含接受者为 *T 或 T 的所有方法集
-	// -> 类型 *T 的可调用方法集包含接受者为 *T 的所有方法
-	// -> 类型 *T 的可调用方法集不包含接受者为 T 的方法
 	t.Run("using value type", func(t *testing.T) {
 		var lst List
 		assert.False(t, LongEnough(lst))
